f3ba3c3d25a1668696f737e62ab4177a29ed50c3
unknown <ulsa22992299@hotmail.com>
2022-05-18 10:12:46 +0300

    answer 3

--- array.js
+++ array.js
@@ -1,6 +1,17 @@
 // 1
-const numbers = [] ;
-const sumNumbers = numbers.reduce((accumulator,number)=>{
-    return accumulator+number;
-},0)
+const sumNumbersInArray = (numbersArray) => {
+    return numbersArray.reduce((accumulator, number) => {
+        return accumulator + number;
+    }, 0)
+}
 
+// 2
+
+const squereNumbers = (squereArray) =>{
+    return  squereArray.map(squere=>squere*squere) 
+}
+
+// 3
+const multiNumbers =(multiArray) =>{
+    return multiArray.reduce((num)=> num*num);
+}
