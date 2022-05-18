d9611a34b6a7810b8a9a67930c9cfc175b0b5abb
unknown <ulsa22992299@hotmail.com>
2022-05-18 11:06:31 +0300

    answer 5

--- array.js
+++ array.js
@@ -15,3 +15,18 @@ const squereNumbers = (squereArray) =>{
 const multiNumbers =(multiArray) =>{
     return multiArray.reduce((num)=> num*num);
 }
+
+// 4
+const biggestNumber = (numArray) =>{
+    numArray.reduce((accumulator,num)=>{
+        if (accumulator<num)
+        return num;
+        else return accumulator;
+     
+        
+    },0)
+}
+const biggerThen6 = (biggerArray)=>{
+  return biggerArray.filter((a)=>a>6)
+}
+console.log(biggerThen6([6,7,8]))
